b78c09fc5a698a78c80c46464ff9af0b08a9529c
Dallin Schwartz <dallin.schwartz@gmail.com>
2017-03-31 07:32:29 +0000

    Added time functionality and started model file for page views

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class PageCounter(models.Model):
+    page = models.URLField(default='default')
+    views = models.IntegerField(default=0)
--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import time
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello, world! " + time.strftime('%X'))
