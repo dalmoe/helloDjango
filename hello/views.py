bd402617da320a46abe01e52b102e0da61f65a32
Dallin Schwartz <dallin.schwartz@gmail.com>
2017-03-31 18:01:05 +0000

    Page counter functionality now works and admin user has been created

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
-
+from .models import PageCounter
 
 def index(request):
-    return HttpResponse("Hello, world! " + time.strftime('%X'))
+    counter, create = PageCounter.objects.get_or_create(page='index')
+    counter.views += 1
+    counter.save()
+    return HttpResponse("Hello, world! " + time.strftime('%X') + " " + str(counter.views) + "views")
