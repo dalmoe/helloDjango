5764f18509bdb2e760d9ec8e2fe75cfb1d74c4ea
Dallin Schwartz <dallin.schwartz@gmail.com>
2017-04-01 07:23:59 +0000

    Added .gitignore and GeoIP2 module along with assosiated APIs.  Page now says hello to whatever city the user is from.

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GEOIP_PATH = BASE_DIR + '/django_hello_world/APIs'
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.gis.geoip2 import GeoIP2
 import time
 from .models import PageCounter
 
@@ -7,4 +8,6 @@ def index(request):
     counter, create = PageCounter.objects.get_or_create(page='index')
     counter.views += 1
     counter.save()
-    return HttpResponse("Hello, world! " + time.strftime('%X') + " " + str(counter.views) + "views")
+    client_address = request.META['HTTP_X_FORWARDED_FOR']
+    g = GeoIP2()
+    return HttpResponse("Hello, " + g.city(client_address)["city"] + "!  Server time is: " + time.strftime('%X') + " Page Views: " + str(counter.views))
