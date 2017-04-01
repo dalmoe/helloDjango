3f8781e405691f0c3567b732f9f262c55e4d2824
Dallin Schwartz <dallin.schwartz@gmail.com>
2017-04-01 07:46:40 +0000

    Changed URLField to CharField in page counter model.

--- hello/models.py
+++ hello/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 # Create your models here.
 class PageCounter(models.Model):
-    page = models.URLField(default='default')
+    page = models.CharField(max_length=32, default='default')
+    
     views = models.IntegerField(default=0)
